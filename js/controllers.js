89b483c962527049c5588dd8ee0c1b318c677d11
sebastienbw <sebastienbw@gmail.com>
2015-01-20 13:55:45 -0800

    first commit

---
+++ js/controllers.js
@@ -0,0 +1,12 @@
+var lab2App = angular.module('lab2App', []);
+
+lab2App.controller('PhoneListCtrl', function ($scope) {
+  $scope.phones = [
+    {'name': 'Nexus S',
+     'snippet': 'Fast just got faster with Nexus S.'},
+    {'name': 'Motorola XOOM™ with Wi-Fi',
+     'snippet': 'The Next, Next Generation tablet.'},
+    {'name': 'MOTOROLA XOOM™',
+     'snippet': 'The Next, Next Generation tablet.'}
+  ];
+});
